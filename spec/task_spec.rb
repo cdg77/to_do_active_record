8b02dff85c5e55a728c0f81b76a0e36c079329bc
Carlo <cdegregorio@gmail.com>
2015-08-24 16:31:07 -0700

    it will validate that a description is no more than 50 characters long.(5 passing specs)

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -13,6 +13,11 @@ describe(Task) do
     expect(task.save()).to(eq(false))
   end
 
+  it('ensures the description length is at most 50 characters') do
+    task = Task.new({:description => 'a'.*(51)})
+    expect(task.save()).to(eq(false))
+  end
+
   describe(".not_done") do
       it("returns the not done tasks") do
         not_done_task1 = Task.create({ :description => "gotta do it", :done => false })
